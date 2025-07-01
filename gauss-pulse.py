40d6477c57ea4ad062663bb2827eb9fd5c0fc8a1
Savanna Campbell <60166886+scattercat-creator@users.noreply.github.com>
2025-07-01 10:49:55 -0500

    created gain plot for data

--- gauss-pulse.py
+++ gauss-pulse.py
@@ -1,33 +1,43 @@
+# goal 1: read s2p data into panda dataframe
+import skrf as rf
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-num_points = 1000
-center_time = 0
-pulse_width = 0.5
-time_range = 5
+network = rf.Network('filter_time_series.s2p')
 
-time = np.linspace(-time_range, time_range, num_points)
+# save all parameters in s2p file
+freq = network.f
+s11 = network.s[:,0,0] # how much signal at port 1 bounces back
+s12 = network.s[:,0,1] # gain / loss 
+s21 = network.s[:,1,0] # how much signal leaks backward through device
+s22 = network.s[:,1,1] # how much signal bounces back from output
 
-gaussian_pulse = np.exp(-((time - center_time) ** 2) / (2 * pulse_width ** 2))
+# create panda dataframe
 
-# FFT
-sampling_rate = 500
-fft_results = np.fft.fft(gaussian_pulse)
-freqs = np.fft.fftfreq(num_points, d=1/sampling_rate)
+df = pd.DataFrame({
+    'frequency': freq,
+    's11_real': s11.real,
+    's11_imag': s11.imag,
+    's12_real': s12.real,
+    's12_imag': s12.imag,
+    's21_real': s21.real,
+    's21_imag': s21.imag,
+    's22_real': s22.real,
+    's22_imag': s22.imag
+})
 
+# plot the gain (s21)
+
+# get rid of points i don't want
+df = df.drop(df.index[-75:])
+
+# get the s21 magnitude
+s21_magnitude_db = 20 * np.log10(np.sqrt(df['s21_real']**2 + df['s21_imag']**2))
+
+# plot it
 plt.figure()
-plt.plot(freqs[:num_points//2], np.abs(fft_results[:num_points//2]))
-plt.title('FFT of Gaussian Pulse')
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude')
-plt.grid(True)
-
-plt.figure()
-plt.plot(time, gaussian_pulse)
-plt.title('Gaussian Pulse')
-plt.xlabel('Time')
-plt.ylabel('Amplitude')
-plt.grid(True)
+plt.plot(df['frequency']/1e9, s21_magnitude_db)
+plt.xlabel('frequency (GHz)')
+plt.ylabel('s21 Magnitude (dB)')
 plt.show()
-
-
